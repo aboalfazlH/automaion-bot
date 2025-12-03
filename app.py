5ff09aca3718f1e3deca19a367511db725ad00c4
seyed aboalfazl hashemi <saboalfazl6162@gmail.com>
2025-12-03 10:20:00 +0330

    Debug

--- app.py
+++ app.py
@@ -79,4 +79,4 @@ def webhook():
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 443)))
